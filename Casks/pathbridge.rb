c9d6f86c0f180e0aa5fcde0bd884a587bec1edb3
lzy <zhiyuan.liang@opay-inc.com>
2026-03-02 17:40:15 +0800

    pathbridge 0.2.4

--- Casks/pathbridge.rb
+++ Casks/pathbridge.rb
@@ -1,8 +1,8 @@
 cask "pathbridge" do
-  version "0.2.0"
-  sha256 "af6225eeee6082f55a9ea753f6e9102ceabc926f9891d746fcf47b0ca9547d2b"
+  version "0.2.4"
+  sha256 "8afe226b2e04e8db1f5af3df044067e7ed5da67cde8a50da378d5659bdc0120d"
 
-  url "https://github.com/lzy0809/PathBridge/releases/download/v#{version}/PathBridge_v0.2.0.dmg"
+  url "https://github.com/lzy0809/PathBridge/releases/download/v#{version}/PathBridge_v0.2.4.dmg"
   name "PathBridge"
   desc "Open Finder directory in your selected terminal"
   homepage "https://github.com/lzy0809/PathBridge"
